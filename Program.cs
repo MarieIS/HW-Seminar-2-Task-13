28fbfd3f06ccde396bf542cac20ddb28ef826f69
Maria <mariya-ivanova@list.ru>
2024-03-06 21:56:42 +0200

    the solution was just updated

--- Program.cs
+++ Program.cs
@@ -7,13 +7,13 @@ if (N / 100 < 1)
  }
  else
   {
-    while (N / 100 >= 1)
+    while (N / 10 >= 10)
      {
-      if (N / 100 <= 9)
+      if (N / 10 <= 99)
         {
          int digit3 = N % 10;
          Console.WriteLine($"{M} -> {digit3}");
         }
-       N = N / 100;
+       N = N / 10;
      }     
   }
