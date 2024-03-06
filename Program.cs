e296a3e72f93464e65924cfeb0affa0e7e5d90a6
Maria <mariya-ivanova@list.ru>
2024-03-06 21:25:23 +0200

    the solution was just added

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Insert a positive number: ");
+int N = int.Parse(Console.ReadLine());
+int M = N;
+if (N / 100 < 1)
+ {
+   Console.WriteLine($"{N} -> no third digit");
+ }
+ else
+  {
+    while (N / 100 >= 1)
+     {
+      if (N / 100 <= 9)
+        {
+         int digit3 = N % 10;
+         Console.WriteLine($"{M} -> {digit3}");
+        }
+       N = N / 100;
+     }     
+  }
